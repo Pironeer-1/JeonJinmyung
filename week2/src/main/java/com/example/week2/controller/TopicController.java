817b4772bceb78b74d5b9ed84fbe4a90ce0c89ac
JNMYNG <jmj00327@gmail.com>
2024-09-20 12:35:34 +0900

    swagger 꾸미기

--- week2/src/main/java/com/example/week2/controller/TopicController.java
+++ week2/src/main/java/com/example/week2/controller/TopicController.java
@@ -4,6 +4,8 @@ import com.example.week2.dto.request.TopicCreateRequest;
 import com.example.week2.dto.request.TopicUpdateRequest;
 import com.example.week2.dto.response.TopicResponse;
 import com.example.week2.service.TopicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,17 +14,20 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "게시물(Topic)")
 @RequestMapping("/api/topic")
 
 public class TopicController {
     private final TopicService topicService;
     @PostMapping
+    @Operation(summary = "게시물 작성")
     public ResponseEntity<?> create(@RequestBody TopicCreateRequest request) {
         topicService.save(request);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{topicId}")
+    @Operation(summary = "게시물 단건 조회")
     public ResponseEntity<TopicResponse> read(@PathVariable("topicId") Long id){
         TopicResponse response = topicService.findById(id);
         return ResponseEntity.ok().body(response);
@@ -30,6 +35,7 @@ public class TopicController {
 
     //전체 조회
     @GetMapping
+    @Operation(summary = "게시물 전체 조회")
     public ResponseEntity<List<TopicResponse>> readAll(){
         List<TopicResponse> responses = topicService.findAll();
         return ResponseEntity.ok().body(responses);
@@ -37,6 +43,7 @@ public class TopicController {
 
     //수정
     @PatchMapping
+    @Operation(summary = "게시물 수정")
     public ResponseEntity<TopicResponse> update(@RequestBody TopicUpdateRequest request) {
         TopicResponse response = topicService.update(request);
         return ResponseEntity.ok().body(response);
@@ -44,6 +51,7 @@ public class TopicController {
 
     //삭제
     @DeleteMapping("/{topicId}")
+    @Operation(summary = "게시물 삭제")
     public ResponseEntity<?> remove(@PathVariable("topicId") Long id){
         topicService.deleteById(id);
         return ResponseEntity.ok().build();
